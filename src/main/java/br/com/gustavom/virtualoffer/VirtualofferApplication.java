667f96a66c6f17de6bea24afebdce5998b8deb18
Gustavo Martins <gustavommartinsctg@outlook.com>
2022-10-26 00:03:57 -0300

    Start project

---
+++ src/main/java/br/com/gustavom/virtualoffer/VirtualofferApplication.java
@@ -0,0 +1,13 @@
+package br.com.gustavom.virtualoffer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualofferApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VirtualofferApplication.class, args);
+    }
+
+}
