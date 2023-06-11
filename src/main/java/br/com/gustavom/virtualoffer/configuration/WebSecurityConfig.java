3ee33b65fa1c24e27fe532384c08f8bf5f31758b
Gustavo Martins <gustavommartinsctg@outlook.com>
2023-06-11 00:03:14 -0300

    Criando autenticação com JDBC

--- src/main/java/br/com/gustavom/virtualoffer/configuration/WebSecurityConfig.java
+++ src/main/java/br/com/gustavom/virtualoffer/configuration/WebSecurityConfig.java
@@ -1,19 +1,29 @@
 package br.com.gustavom.virtualoffer.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import javax.sql.DataSource;
+
 @Configuration
-@EnableWebSecurity
 public class WebSecurityConfig {
+
+    final DataSource dataSource;
+    private static final String ADMINISTRADOR =  "admin";
+
+    @Autowired
+    public WebSecurityConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -24,20 +34,37 @@ public class WebSecurityConfig {
                         .authenticated()
                 ).formLogin(login -> login
                         .loginPage("/login")
-                        .successForwardUrl("/")
+                        .defaultSuccessUrl("/", true)
                         .permitAll())
                 .logout(logout -> logout
                         .logoutUrl("/logout")).build();
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("gustavo")
-                        .password("123")
-                        .roles("USER")
-                        .build();
-        return new InMemoryUserDetailsManager(user);
+    public UserDetailsManager users() {
+        UserDetailsManager users = new JdbcUserDetailsManager(this.dataSource);
+        criaUsuarioDefaultSeNaoExistirUsuarios(users);
+        return users;
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    private void criaUsuarioDefaultSeNaoExistirUsuarios(UserDetailsManager user) {
+        if(!user.userExists(ADMINISTRADOR)){
+            user.createUser(createDefaulUser());
+        }
+    }
+
+    private UserDetails createDefaulUser() {
+        PasswordEncoder encoder = passwordEncoder();
+        return User.builder()
+                .username(ADMINISTRADOR)
+                .password(encoder.encode(ADMINISTRADOR))
+                .roles("ADMIN")
+                .build();
+    }
+
 }
